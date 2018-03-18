3da0e24546541e7d9195063e6cc5da711efcce5c
Mario Lang <mlang@delysid.org>
2018-03-18 15:03:01 +0100

    Reorder commands

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ module Main where
 
 import Diagrams.Backend.Braille.CmdLine
 import Diagrams.Backend.CmdLine (Parseable(..))
-import Diagrams.Prelude hiding (option)
+import Diagrams.Prelude hiding (option, value)
 import Options.Applicative
 
 d = f 1 `atop` f (sqrt 2 / 2) `atop` f (sqrt 2 * (sqrt 2 / 2) / 2) where
@@ -35,25 +35,50 @@ newtype Opts = Opts { draw :: Diagram B }
 
 instance Parseable Opts where
   parser = fmap (Opts . mconcat) $ some $ hsubparser $ mconcat [
-      cmd "circle" "A circle of the given radius, centered at the origin." $
+      commandGroup "Diagrams"
+    , cmd "hrule" "A centered horizontal (L-R) line of the given length." $
+      hrule <$> arg (metavar "LENGTH")
+    , cmd "vrule" "A centered vertical (T-B) line of the given length." $
+      vrule <$> arg (metavar "LENGTH")
+    , cmd "triangle" "An equilateral triangle, with sides of the given length and base parallel to the x-axis." $
+      triangle <$> arg (metavar "LENGTH")
+    , cmd "rect" "An axis-aligned rectangle of the given width and height, centered at the origin." $
+      rect <$> arg (metavar "WIDTH")
+           <*> arg (metavar "HEIGHT")
+    , cmd "roundedRect" "An axis-aligned rectangle with the given width and height with circular rounded corners of radius, centered at the origin." $
+      roundedRect <$> arg (metavar "WIDTH")
+                  <*> arg (metavar "HEIGHT")
+                  <*> arg (metavar "RADIUS")
+    , cmd "square" "A square with its center at the origin and sides of the given length, oriented parallel to the axes." $
+      square <$> arg (metavar "LENGTH")
+    , cmd "pentagon" "A regular pentagon, with sides of the given length and base parallel to the x-axis." $
+      pentagon <$> arg (metavar "LENGTH")
+    , cmd "hexagon" "A regular hexagon, with sides of the given length and base parallel to the x-axis." $
+      hexagon <$> arg (metavar "LENGTH")
+    , cmd "heptagon" "A regular heptagon, with sides of the given length and base parallel to the x-axis." $
+      heptagon <$> arg (metavar "LENGTH")
+    , cmd "octagon" "A regular octagon, with sides of the given length and base parallel to the x-axis." $
+      octagon <$> arg (metavar "LENGTH")
+    , cmd "nonagon" "A regular nonagon, with sides of the given length and base parallel to the x-axis." $
+      nonagon <$> arg (metavar "LENGTH")
+    , cmd "decagon" "A regular decagon, with sides of the given length and base parallel to the x-axis." $
+      decagon <$> arg (metavar "LENGTH")
+    , cmd "hendecagon" "A regular hendecagon, with sides of the given length and base parallel to the x-axis." $
+       hendecagon <$> arg (metavar "LENGTH")
+    , cmd "dodecagon" "A regular dodecagon, with sides of the given length and base parallel to the x-axis." $
+      dodecagon <$> arg (metavar "LENGTH")
+    , cmd "circle" "A circle of the given radius, centered at the origin." $
       circle <$> arg (metavar "RADIUS")
-    , cmd "clock" "A clock showing HOUR and MINUTE." $
-      clock <$> arg (metavar "HOUR") <*> arg (metavar "MINUTE")
-    , cmd "decagon" "" $ decagon <$> arg mempty
-    , cmd "dodecagon" "" $ dodecagon <$> arg mempty
-    , cmd "flower-of-life" "The flower of life." $ pure fol
-    , cmd "hendecagon" "" $ hendecagon <$> arg mempty
-    , cmd "heptagon" "" $ heptagon <$> arg mempty
-    , cmd "hexagon" "" $ hexagon <$> arg mempty
-    , cmd "hilbert" "A hilbert curve of the given ORDER." $
+    , cmd "text" "Print text." $ text <$> strArgument (metavar "STRING")
+    , cmd "baselineText" "Print text." $ baselineText <$> strArgument (metavar "STRING")
+    , cmd "clock" "A clock showing the time given in hours and minutes." $
+      clock <$> arg (metavar "HOUR")
+            <*> arg (metavar "MINUTE")
+    , cmd "flower-of-life" "The \"flower of life\"." $
+      pure fol
+    , cmd "hilbert" "A hilbert curve of the given order." $
       strokeT . hilbert <$> arg (metavar "ORDER")
-    , cmd "nonagon" "" $ nonagon <$> arg mempty
-    , cmd "octagon" "" $ octagon <$> arg mempty
-    , cmd "pentagon" "" $ pentagon <$> arg mempty
-    , cmd "rect" "A rectangle." $ rect <$> arg mempty <*> arg mempty
     , cmd "seed-of-life" "The \"seed of life\"." $ pure sol
-    , cmd "square" "" $ square <$> arg mempty
-    , cmd "triangle" "" $ triangle <$> arg mempty
     ] where cmd s d p = command s $ info (foldr ($) <$> p <*> many modifier) $
                         progDesc d
             arg = argument auto
@@ -63,8 +88,13 @@ modifier = flag' alignL (long "alignL") <|> flag' alignR (long "alignR")
        <|> flag' centerX (long "centerX" <> help "Center the local origin along the X-axis.")
        <|> flag' centerY (long "centerY" <> help "Center the local origin along the Y-axis.")
        <|> flag' centerXY (long "centerXY" <> help "Center along both the X- and Y-axes.")
+       <|> (named :: String -> Diagram B -> Diagram B) <$> option auto (long "named" <> metavar "NAME")
+       <|> flag' reflectX (long "reflectX" <> help "Flip a diagram from left to right, i.e. send the point (x,y) to (-x,y).")
+       <|> flag' reflectY (long "reflectY" <> help "Flip a diagram from top to bottom, i.e. send the point (x,y) to (x,-y).")
+       <|> flag' reflectXY (long "reflectXY" <> help "Flips the diagram about x=y, i.e. send the point (x,y) to (y,x).")
        <|> rotateBy <$> option auto (long "rotateBy")
        <|> scaleX <$> option auto (long "scaleX" <> metavar "FACTOR" <> help "Scale a diagram by the given factor in the x (horizontal) direction.")
+       <|> scaleY <$> option auto (long "scaleY" <> metavar "FACTOR" <> help "Scale a diagram by the given factor in the y (vertical) direction.")
        <|> flag' showOrigin (long "showOrigin")
        <|> translateX <$> option auto (long "translateX")
        <|> translateY <$> option auto (long "translateY")
